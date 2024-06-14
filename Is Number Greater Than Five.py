682907750dfa9c7d952b2e63bf0690d4d6a7a757
Jayesh <116382728+Jv2350@users.noreply.github.com>
2024-06-14 23:22:27 +0530

    Create Is Number Greater Than Five.py

---
+++ Is Number Greater Than Five.py
@@ -0,0 +1,8 @@
+# rite a function to check if a given number isreater than 5.
+# Instructions
+# If the number is greater than 5, return True. Otherwise, return False.
+
+
+def is_greater_than_five(n):
+    return True if n > 5 else False
+
