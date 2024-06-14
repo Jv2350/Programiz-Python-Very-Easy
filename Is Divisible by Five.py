aeaac6b49006ccd1ab36b0765df01d27fcc9e3b3
Jayesh <116382728+Jv2350@users.noreply.github.com>
2024-06-14 23:22:47 +0530

    Create Is Divisible by Five.py

---
+++ Is Divisible by Five.py
@@ -0,0 +1,10 @@
+# Write a function to check if a number is divisible by five.
+# Instructions
+# Return True if the number is divisible by 5. Otherwise, return False.
+
+
+def is_divisible_by_five(n):
+    if n % 5 == 0:
+        return True
+    else:
+        return False
