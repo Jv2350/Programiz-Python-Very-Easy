3426b2b4a91ca075345021a847c124a9702d6dda
Jayesh <116382728+Jv2350@users.noreply.github.com>
2024-06-14 23:21:48 +0530

    Create Can Enter Club.py

---
+++ Can Enter Club.py
@@ -0,0 +1,13 @@
+# Write a function to determine if a person can enter a club.
+# Instructions
+# The club only allows people who are 21 years old or older.
+# Return True if the person can enter the club. Otherwise, return False if they cannot enter the club.
+
+
+def can_enter_club(age):
+    return True if age >= 21 else False
+
+    if age >= 21:
+        return True
+    else:
+        return False
