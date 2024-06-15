a7aa50076760bdfab304207dd60811daba716af6
Jayesh <116382728+Jv2350@users.noreply.github.com>
2024-06-15 22:52:55 +0530

    Create Is Odd or Even.py

---
+++ Is Odd or Even.py
@@ -0,0 +1,8 @@
+# Write a function to check if the enteredinteger is odd or even.
+# Instructions
+# If the given number is odd, return "Odd"
+# If the given number is even, return "Even"
+
+
+def odd_even(num):
+    return "Even" if num % 2 == 0 else "Odd"
