968eea0daa13366d5292aab6422ee05bc0c0bbab
Jayesh <116382728+Jv2350@users.noreply.github.com>
2024-06-18 23:27:34 +0530

    Create Is Number Even.py

---
+++ Is Number Even.py
@@ -0,0 +1,7 @@
+# Write a function to check if a given number is even or not.
+# Instructions
+# Return True if the number is even, otherwise return False.
+
+
+def is_even(n):
+    return True if n % 2 == 0 else False
