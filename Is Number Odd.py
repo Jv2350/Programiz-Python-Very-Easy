d20d47c4a0a7f329030df5b207798ad048500966
Jayesh <116382728+Jv2350@users.noreply.github.com>
2024-06-18 23:28:00 +0530

    Create Is Number Odd.py

---
+++ Is Number Odd.py
@@ -0,0 +1,7 @@
+# Write a function to check if a number is odd.
+# Instructions
+# Return True if the number is odd, otherwise return False.
+
+
+def is_odd(n):
+    return True if n % 2 != 0 else False
